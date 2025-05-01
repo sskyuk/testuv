288996c7a7f16399cef3911b8214e2959d17d177
sskyuk <148633111+sskyuk@users.noreply.github.com>
2025-05-01 20:04:23 +0900

    first commit

---
+++ src/for.py
@@ -0,0 +1,7 @@
+num = 0
+
+for i in range(100):
+    addnum = i + 1
+    num += addnum # num = num + addnum
+
+print(num)
---
+++ src/if.py
@@ -0,0 +1,11 @@
+inputnum = int(input("Please enter 0 or 1:"))
+
+if inputnum == 0:
+    print("You entered zero.")
+
+elif inputnum == 1:
+    print("You entered one.")
+
+else:
+    print("Your input number is incorrect.")
+
